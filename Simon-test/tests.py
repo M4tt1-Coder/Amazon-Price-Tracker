fd54edda9751204d524ca25043d0d2a35ab392ab
Simon <simon_julian.schairer@studmail.htw-aalen.de>
2024-12-06 12:02:19 +0100

    price scrapper v1 with bfs

--- Simon-test/tests.py
+++ Simon-test/tests.py
@@ -1,3 +1,20 @@
-print("hello world")
-print("test")
+import requests
+import bs4 as bs
+import lxml
+
+Url ="https://www.amazon.de/Edelstahlkette-langgliedrig-%C3%A4hnlich-DIN763-Meter/dp/B07Z96FR8N?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=25KG0EIKCAM7C&dib=eyJ2IjoiMSJ9.mVbN-QEoe_syptOIlx_bnkQTn48sW5Zy-Opq1vA9fKiTjPVKdJVmoh4_z4on526fslUCPLZIWi7fdsSRpjDekvY_69Fr_X4-abZ_ubcakfWsYOVcnFGkJBQZdZ4kjdZjCua5RyXJvlusGa-L7rAwXFyA3jW2ElmWCbCNOee-RZTQ7gJymaznN8fty1wDfSXdebQDmwLsYeXq2H2OfsVLGxNkT4QrCYZEyEm3C6D7zNQSXICPjVCXuHOhDvPqVD1acCK11SfiMqPBU8a19Zru6BWzRn4PKv4RQwsJDIM9_Tc.0ZD0ufATMp9zaAWrE-iSQOz_dydQ6naJ5hgfGcTalvs&dib_tag=se&keywords=ketten&qid=1733481248&sprefix=ketten%2Caps%2C141&sr=8-9"
+scrap = requests.get(Url)
+if scrap.status_code == 200:
+    inthalt=scrap.text
+    print("scraping erfolgreich")
+else:
+    print("error")
+#try:
+inthalt_pars = bs.BeautifulSoup(inthalt, features="lxml")
+target_price= inthalt_pars.find("span", attrs={"class": "a-price-whole"})
+price = target_price.get_text()
+print(price)
+
+#except Exception as e:
+ #   print("fehler ist aufgetreten")
 
