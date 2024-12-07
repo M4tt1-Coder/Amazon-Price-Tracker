59d7d5816038292dcc9846cbead9b546f932fee1
Simon <simon_julian.schairer@studmail.htw-aalen.de>
2024-12-07 16:59:47 +0100

    added input to url for testing

--- Simon-test/tests.py
+++ Simon-test/tests.py
@@ -2,13 +2,14 @@ import requests
 import bs4 as bs
 import lxml
 
-Url ="https://www.amazon.de/Edelstahlkette-langgliedrig-%C3%A4hnlich-DIN763-Meter/dp/B07Z96FR8N?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=25KG0EIKCAM7C&dib=eyJ2IjoiMSJ9.mVbN-QEoe_syptOIlx_bnkQTn48sW5Zy-Opq1vA9fKiTjPVKdJVmoh4_z4on526fslUCPLZIWi7fdsSRpjDekvY_69Fr_X4-abZ_ubcakfWsYOVcnFGkJBQZdZ4kjdZjCua5RyXJvlusGa-L7rAwXFyA3jW2ElmWCbCNOee-RZTQ7gJymaznN8fty1wDfSXdebQDmwLsYeXq2H2OfsVLGxNkT4QrCYZEyEm3C6D7zNQSXICPjVCXuHOhDvPqVD1acCK11SfiMqPBU8a19Zru6BWzRn4PKv4RQwsJDIM9_Tc.0ZD0ufATMp9zaAWrE-iSQOz_dydQ6naJ5hgfGcTalvs&dib_tag=se&keywords=ketten&qid=1733481248&sprefix=ketten%2Caps%2C141&sr=8-9"
+Url = input("Enter URL: ")
 scrap = requests.get(Url)
 if scrap.status_code == 200:
     inhalt=scrap.text
     print("scraping erfolgreich")
 else:
     print("error")
+    exit()
 #try:
 inhalt_pars = bs.BeautifulSoup(inhalt, features="lxml")
 target_price_full= inhalt_pars.find("span", attrs={"class": "a-price-whole"})
