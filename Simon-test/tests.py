d3c37553ad4b533e52f92e276ad37f4282130e50
Simon <simon_julian.schairer@studmail.htw-aalen.de>
2024-12-10 13:17:32 +0100

    deletet some code for easier testing and added a error message to prevent crashing

--- Simon-test/tests.py
+++ Simon-test/tests.py
@@ -4,23 +4,21 @@ import lxml
 
 Url = input("Enter URL: ")
 scrap = requests.get(Url)
-if scrap.status_code == 200:
-    inhalt=scrap.text
-    print("scraping erfolgreich")
-else:
-    print("error")
-    exit()
+inhalt=scrap.content
+print("scraping erfolgreich")
+
 #try:
-inhalt_pars = bs.BeautifulSoup(inhalt, features="lxml")
-target_price_full= inhalt_pars.find("span", attrs={"class": "a-price-whole"})
-price_full = target_price_full.get_text()
-target_price_fraction = inhalt_pars.find("span", attrs={"class": "a-price-fraction"})
-price_fraction = target_price_fraction.get_text()
-print(target_price_fraction)
-price=price_full+price_fraction
-
-
+inhalt_pars = bs.BeautifulSoup(inhalt, features="lxml")#
+target_price= inhalt_pars.find('span', attrs={'class':'a-price-whole'})
+price = 0
+if target_price is not None:
+    price = target_price.text
+else:
+    price = "suche des preises nicht erfolgreich"
 print(price)
+
+
+
 
 #except Exception as e:
  #   print("fehler ist aufgetreten")
