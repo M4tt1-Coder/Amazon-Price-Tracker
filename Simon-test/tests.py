77e60374fa3cac4174f38a52b236b8d872ae4454
Simon <simon_julian.schairer@studmail.htw-aalen.de>
2024-12-10 21:46:32 +0100

    fixed price scrapper(just works once)

--- Simon-test/tests.py
+++ Simon-test/tests.py
@@ -3,19 +3,24 @@ import bs4 as bs
 import lxml
 
 Url = input("Enter URL: ")
-header = ({'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-            'Accept-Language': 'en-US, en;q=0.5'})
+header = ({'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0',
+                    'Accept-Language':'en-US,en;q=0.5',
+                    'Sec-Fetch-Dest':'document',
+                    'Sec-Fetch-Mode':'navigate',
+                    'Sec-Fetch-Site':'same-origin',
+                    'Sec-Fetch-User':'?1',
+                    'Upgrade-Insecure-Requests':'1'})
 scrap = requests.get(Url,headers=header)
-inhalt=scrap.content
+inhalt=scrap.text
 print("scraping erfolgreich")
 
 #try:
-inhalt_pars = bs.BeautifulSoup(inhalt, features="lxml")#
+inhalt_pars = bs.BeautifulSoup(inhalt, features="lxml")
+#print(inhalt_pars)
 target_price_full= inhalt_pars.find('span', attrs={'class':'a-price-whole'})
 target_price_fraction= inhalt_pars.find('span', attrs={'class':'a-price-fraction'})
 price = 0
-if target_price_full is not None:
+if target_price_full:
     price_full = target_price_full.text
     price_fraction = target_price_fraction.text
     price = price_full + price_fraction
