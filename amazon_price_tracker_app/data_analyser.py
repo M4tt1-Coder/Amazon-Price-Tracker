73a9a60ede075de45168f7607f2b46d69a2bf631
Jannis Gäse <jannisgase09@gmail.com>
2024-12-16 15:27:54 +0100

    added product variable

--- amazon_price_tracker_app/data_analyser.py
+++ amazon_price_tracker_app/data_analyser.py
@@ -5,14 +5,15 @@ from random import random
 def get_data_np():      # declare function to test program
     price = random()    # random 'price' value for testing
     date = random()     # random 'date' value for testing
-    return price, date  # return 'price' and 'date' values
+    product = random()     # random 'date' value for testing
+    return price, date, product  # return 'price' and 'date' values
 
 def receive_data_np():      # declare function for receiving return values from 'get_data_np()' function
-    p, d = get_data_np()    # save the two return values of 'get_data_np()' function in 'p' and 'd' 
-
-    data = {'price': [p], 'date': [d]}  # create a dictionary with your values
+    price, date, product = get_data_np()    # save the two return values of 'get_data_np()' function in 'product', 'date' and 'product'
+    data = {'price': [price], 'date': [date], 'product': [product]}  # create a dictionary with your values
     df = pd.DataFrame(data)             # convert to DataFrame
-    print(p)                            # print 'p' value in terminal to compare, if it is the same value, that is written into the excel
-    print(d)                            # print 'd' value in terminal to compare, if it is the same value, that is written into the excel
+    print(price)                            # print 'price' value in terminal to compare, if it is the same value, that is written into the excel
+    print(date)                            # print 'date' value in terminal to compare, if it is the same value, that is written into the excel
+    print(product)                            # print 'product' value in terminal to compare, if it is the same value, that is written into the excel
     df.to_excel('output.xlsx', index=False)
 receive_data_np()
