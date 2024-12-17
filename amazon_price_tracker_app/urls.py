2227c48b2a6331a435868f5fe9cdb3c43eb00865
M4tt1-Coder <geimat75@gmail.com>
2024-12-17 18:41:23 +0100

    Styling with TailwindCSS in Django
    We will style our HTML elements with TailwindCSS so I added it as dependency.

--- amazon_price_tracker_app/urls.py
+++ amazon_price_tracker_app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-# TODO - Databases are not neccessary -> we get the data on runtime from the API
-
 urlpatterns = [
-    path('/', views.home, name="home")
+    path('', views.home, name="home")
 ]
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./amazon_price_tracker_app/templates/**/*.html', './**/templates/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
