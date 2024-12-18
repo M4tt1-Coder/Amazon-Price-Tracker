0678a779f313230e9a98da72f04eb55cfd2012c6
XEALAE <maxsteingraeber@gmail.com>
2024-12-18 09:56:17 +0100

    now every call of function 'receive_data_np()' saves the new data under the old data and doesn't reset it

--- amazon_price_tracker_app/data_analyser.py
+++ amazon_price_tracker_app/data_analyser.py
@@ -2,17 +2,37 @@ import pandas as pd
 from random import random
 
 # 2 random values
-def get_data_np():      # declare function to test program
-    price = random()    # random 'price' value for testing
-    date = random()     # random 'date' value for testing
-    return price, date  # return 'price' and 'date' values
+def get_data_np():  # declare function to test program
+    price = random()      # random 'price' value for testing
+    date = random()       # random 'date' value for testing
+    product = random()    # random 'product' value for testing
+    return price, date, product
 
-def receive_data_np():      # declare function for receiving return values from 'get_data_np()' function
-    p, d = get_data_np()    # save the two return values of 'get_data_np()' function in 'p' and 'd' 
+def receive_data_np():  
+    # declare variables for the return values of 'get_data_np()'
+    price, date, product = get_data_np()  
 
-    data = {'price': [p], 'date': [d]}  # create a dictionary with your values
-    df = pd.DataFrame(data)             # convert to DataFrame
-    print(p)                            # print 'p' value in terminal to compare, if it is the same value, that is written into the excel
-    print(d)                            # print 'd' value in terminal to compare, if it is the same value, that is written into the excel
-    df.to_excel('output.xlsx', index=False)
+    # create DataFrame with columns
+    product_data = pd.DataFrame([[price, date, product]], columns=['price', 'date', 'product'])
+
+    try:
+        # load existing 'product_data.xlsx'
+        header = pd.read_excel('amazon_product_data.xlsx')
+    except FileNotFoundError:
+        # if 'product_data.xlsx' doesn't exist start with new DataFrame
+        header = pd.DataFrame(columns=['price', 'date', 'product'])
+
+    # add row to existing excel
+    updated_data = pd.concat([header, product_data], ignore_index=True)
+
+    # write 'updated_data' into excel
+    updated_data.to_excel('amazon_product_data.xlsx', index=False, engine='openpyxl')
+
+    # terimnal output
+    print("new files were added successfully:")
+    print(f"price: {price}, date: {date}, product: {product}")
+
+# test function
+receive_data_np()
+receive_data_np()
 receive_data_np()
