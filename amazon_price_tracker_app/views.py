6f4230943600ffdbb79e91477b09ffe475429b36
Simon <simon_julian.schairer@studmail.htw-aalen.de>
2025-01-03 13:38:39 +0100

    fixed everything, submit and delete now working

--- amazon_price_tracker_app/views.py
+++ amazon_price_tracker_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 import pandas as pd
 from .forms import  urlform
 import os
@@ -15,22 +15,26 @@ def input(request):
         settings.BASE_DIR,
         "amazon_price_tracker_app/data/urls.txt"
     )
+    with open(data_file_path, "r") as txt:
+        data = txt.read()
     if request.method == "POST":
         form = urlform(request.POST)
         if form.is_valid():
             url = form.cleaned_data['user_input']
-            if request.POST.get("action")=="submit":
+            if request.POST.get("submit")=="submit":
                 with open(data_file_path, "a") as file:
                     file.write(url+"\n")
-            elif request.POST.get("action")=="delete":
-                with open(data_file_path, "w") as file:
-                    for line in file:
-                        if line!=url:
-                            file.write(line)
+                return redirect("input") #update site to show new list
+            elif request.POST.get("submit")=="delete":
+                with open(data_file_path, "r") as file:
+                    lines=file.readlines()
+                with open(data_file_path, "w") as write:
+                    for line in lines:
+                            if line.strip("\n")!=url:
+                                write.write(line)
+                return redirect("input")#update site to show new list
 
 
     else: form = urlform()
-    with open(data_file_path, "r+") as txt:
-        data = txt.read()
 
     return render(request, 'input.html',{"form":form,"products":data})
