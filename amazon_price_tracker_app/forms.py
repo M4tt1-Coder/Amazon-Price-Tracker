feb5cc93746f260cfbe84c1bf36465f29f332867
Simon <simon_julian.schairer@studmail.htw-aalen.de>
2025-01-07 11:07:44 +0100

    minor changes for testing purpose

--- amazon_price_tracker_app/forms.py
+++ amazon_price_tracker_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class urlform(forms.Form):
-    user_input=forms.URLField(label="enter URL",initial="https://www.amazon.de/",help_text="here you can enter your url")
+    user_input=forms.URLField(label="enter URL",initial="https://www.amazon.de/",help_text="here you can enter your url",
+                              widget=forms.URLInput(attrs={'class':''}))
