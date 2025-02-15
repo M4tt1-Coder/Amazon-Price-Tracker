7a6b2c01ec85059b140de9d07d88eff39e47339c
Jannis Gäse <jannisgase09@gmail.com>
2025-02-15 08:53:02 +0100

    a

--- amazon_price_tracker_app/forms.py
+++ amazon_price_tracker_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class urlform(forms.Form):
     user_input=forms.URLField(label="enter URL",initial="https://fakestoreapi.com/products/",help_text="here you can enter your url",
-                              widget=forms.URLInput(attrs={'class':'bg-blue-500 width-800'}))
+                              widget=forms.URLInput(attrs={'class':'bg-black width-800 p-2 my-3 rounded-lg text-white'}))
 
 # https://www.geeksforgeeks.org/django-forms/
 # class ModifyProdCmpListForm(forms.Form): 
