5babd68be12dd6e3c5e1a7b6e94b9aa513544b41
M4tt1-Coder <geimat75@gmail.com>
2025-02-17 19:49:33 +0100

    [ Main (without merge) ] Finished and integrated the GET functions

--- amazon_price_tracker_app/utils.py
+++ amazon_price_tracker_app/utils.py
@@ -3,56 +3,58 @@ import pandas as pd
 from django.conf import settings 
 
 def get_product_by_id(id):
-    excel_file_path = os.path.join(settings.BASE_DIR, "amazon_price_tracker_app/data/amazon_product_data.xlsx")
+    """
+    Uses the id of the product to get the product from the excel file.
+    
+    Args:
+        id (str): ID of the product
 
+    Returns:
+        Product: Product object with the ID of the product
+    """
+    excel_file_path = os.path.join(settings.BASE_DIR, "amazon_price_tracker_app/data/amazon_product_data.xlsx")
     try:
         excel = pd.ExcelFile(excel_file_path)
-
         if id in excel.sheet_names:
             sheet = pd.read_excel(excel_file_path, sheet_name=id)
-
             product = {
                 "id": id,
-                "name": sheet["product"][0],
-                "description": "placeholder",
-                "price": sheet["price"].iloc[-1]
+                "name": sheet["name"][0],
+                "description": sheet.get("description", ["No description"])[0],
+                "price": sheet["price"].iloc[-1],
+                "date": sheet["date"][0]
             }    
             return product
-        
         else:
             return f"Product with ID {id} was not found."
-
     except Exception as e:
         print(f"Error reading Excel file : {str(e)}")
         return None
-    
-#hier beginnt die zweite Funktion 
 
-def get_all_products():
+def get_all_products() -> list:
+    """
+    Fetches all products from the Excel file.
+
+    Returns:
+        list: All products from the Excel file
+    """
     excel_file_path = os.path.join(settings.BASE_DIR, "amazon_price_tracker_app/data/amazon_product_data.xlsx")
-
     all_products = []
-
     try:
         excel = pd.ExcelFile(excel_file_path)
-
         for sheet_name in excel.sheet_names:
             
             sheet = pd.read_excel(excel_file_path, sheet_name=sheet_name)
 
             product = {
                 "id": sheet_name,
-                "name": sheet["product"][0],
+                "name": sheet["name"][0],
                 "description": sheet.get("description", ["No description"])[0],
-                "price": sheet["price"].iloc[-1]
-
+                "price": sheet["price"].iloc[-1],
+                "date": sheet["date"][0]
             }
-
             all_products.append(product)
-        
         return all_products
-        
     except Exception as e:
         print(f"Error reading Excel file: {str(e)}")
         return []
-        
--- amazon_price_tracker_app/views.py
+++ amazon_price_tracker_app/views.py
@@ -1,4 +1,6 @@
 import hashlib
+
+from amazon_price_tracker_app.utils import get_all_products, get_product_by_id
 from .get_productdata import get_data_np
 from .data_analyser import delete_all_products, receive_data_np, delete_excel_sheet
 from django.shortcuts import render, redirect
@@ -11,51 +13,50 @@ from django.conf import settings
 # Create your views here.
 
 # mock data test data
-# TODO - Remove this when we are finished
-mock_products = [
-        {
-            "id": "be81a713-523d-46e1-a4c2-1b52e3f53604",
-            "name": "Product 1",
-            "description": "Some Product 1",
-            "price": 1.99,
-        },
-        {
-            "id": "be81a713-523d-46e1-a4c2-2b52e3f53604",
-            "name": "Product 2",
-            "description": "Some Product 2",
-            "price": 34.99,
-        },
-        {
-            "id": "be81a713-523d-46e1-a4c2-3b52e3f53604",
-            "name": "Product 3",
-            "description": "Some Product 3",
-            "price": 13.59,
-        },
-        {
-            "id": "be81a713-523d-46e1-a4c2-4b52e3f53604",
-            "name": "Product 4",
-            "description": "Some Product 4",
-            "price": 345.99,
-        },
-        {
-            "id": "be81a713-523d-46e1-a4c2-5b52e3f53604",
-            "name": "Product 5",
-            "description": "Some Product 5",
-            "price": 23.00,
-        },
-        {
-            "id": "be81a713-523d-46e1-a4c2-6b52e3f53604",
-            "name": "Product 6",
-            "description": "Some Product 6",
-            "price": 3.99,
-        },
-        {
-            "id": "be81a713-523d-46e1-a4c2-7b52e3f53604",
-            "name": "Product 7",
-            "description": "Some Product 7",
-            "price": 7.49,
-        },
-    ]
+# mock_products = [
+#         {
+#             "id": "be81a713-523d-46e1-a4c2-1b52e3f53604",
+#             "name": "Product 1",
+#             "description": "Some Product 1",
+#             "price": 1.99,
+#         },
+#         {
+#             "id": "be81a713-523d-46e1-a4c2-2b52e3f53604",
+#             "name": "Product 2",
+#             "description": "Some Product 2",
+#             "price": 34.99,
+#         },
+#         {
+#             "id": "be81a713-523d-46e1-a4c2-3b52e3f53604",
+#             "name": "Product 3",
+#             "description": "Some Product 3",
+#             "price": 13.59,
+#         },
+#         {
+#             "id": "be81a713-523d-46e1-a4c2-4b52e3f53604",
+#             "name": "Product 4",
+#             "description": "Some Product 4",
+#             "price": 345.99,
+#         },
+#         {
+#             "id": "be81a713-523d-46e1-a4c2-5b52e3f53604",
+#             "name": "Product 5",
+#             "description": "Some Product 5",
+#             "price": 23.00,
+#         },
+#         {
+#             "id": "be81a713-523d-46e1-a4c2-6b52e3f53604",
+#             "name": "Product 6",
+#             "description": "Some Product 6",
+#             "price": 3.99,
+#         },
+#         {
+#             "id": "be81a713-523d-46e1-a4c2-7b52e3f53604",
+#             "name": "Product 7",
+#             "description": "Some Product 7",
+#             "price": 7.49,
+#         },
+#     ]
 
 
 # page for adding and deleting product urls
@@ -124,9 +125,8 @@ def home(request):
     Returns:
         Context and UI data that is displayed in the viewport
     """
-    # TODO - Implement the getProducts function to fetch products from a data source (API, database, etc.).
     # get all current products
-    products = mock_products
+    products = get_all_products()
     # Set a empty list of product ids
     comparison_product_ids = []
     # if there are already some product ids in the session, load them into the comparison_product_ids list
@@ -215,14 +215,14 @@ def dashboard(request, product_id: str):
     """
     # TODO - Fetch the product from the excel file
     # product = get_product(product_id)
-    mock_product = {
-        "id": 4,
-        "name": "Product 4",
-        "description": "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
-        "price": 345.99,
-        "date": "2015-01-01T00:00:00"
-    }
-    product = mock_product
+    # mock_product = {
+    #     "id": 4,
+    #     "name": "Product 4",
+    #     "description": "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+    #     "price": 345.99,
+    #     "date": "2015-01-01T00:00:00"
+    # }
+    product = get_product_by_id(product_id)
     
     # TODO - Also add here the product plot chart string function
     context = {"product": product, "prod_plot_string": ""}
